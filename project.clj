171cf5ebf79f1408f8422645d5a1cb2aab6226c9
ruroru <111705692+ruroru@users.noreply.github.com>
2025-10-04 10:50:51 +0000

    Stepping project version to prepare it for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.jj/ring-http-exchange "1.2.4-SNAPSHOT"
+(defproject org.clojars.jj/ring-http-exchange "1.2.4"
   :description "Ring adapter for com.sun.net.httpserver"
   :url "https://github.com/ruroru/ring-http-exchange"
   :license {:name "Eclipse Public License"
